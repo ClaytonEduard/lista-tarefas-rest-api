6214c9ac118fb5a733592d71a44ef08c432c78fe
claytoneduard <clayton_eduard@hotmail.com>
2022-05-24 12:06:30 -0300

    Serviço salvar tarefa, lado service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
     @Get()
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
     // criando um array de tarefas
-    private tasks = [];
+    private tasks: Task[] = [];
 
     //servico de listar todas as tarefas
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks;
     }
+
+    // metodo de criar tarefas
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    // para gerar o ID automaticament usamos a biblioteca <yarn add uuid>
+    // comando de iniciar sever, yarn start:dev
 }
