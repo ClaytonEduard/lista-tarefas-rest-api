59c2a03b04f61e33766d893d5d9c9954eb97932e
claytoneduard <clayton_eduard@hotmail.com>
2022-05-25 11:30:01 -0300

    metodo create finalizado

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -8,5 +8,10 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+    @Post()
+    createTask(@Body('title') title: string,
+        @Body('description') description: string): Task {
+        return this.tasksService.createTask(title, description)
     }
 }
