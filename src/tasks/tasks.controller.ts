14ca9ada971f2f09a7335dd3612471b4aba58ee7
claytoneduard <clayton_eduard@hotmail.com>
2022-05-26 12:10:17 -0300

    crud finalizado

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task-dto';
-import { Task } from './tasks.model';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -8,8 +9,13 @@ export class TasksController {
     constructor(private tasksService: TasksService) { }
     // metodo listar do lado do service
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        //se tivermos algum filtro definido basta chamar todas as tarefas
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilter(filterDto);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
     }
     //https://localhost:3000/tasks/id
     @Get('/:id')
@@ -17,12 +23,20 @@ export class TasksController {
         return this.tasksService.getTaskById(id);
     }
 
-
-
-
     // metodo criar do lado do service
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto)
+    }
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        return this.tasksService.deleteTask(id)
+    }
+    // metodo de update
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus,): Task {
+        return this.tasksService.updateTaskStatus(id, status);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -12,6 +13,29 @@ export class TasksService {
     getAllTasks(): Task[] {
         return this.tasks;
     }
+    //servio de listar por filtros
+    getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+        //definir um array temporario com todos os resultados
+        let tasks= this.getAllTasks();
+        //se o status estiver definido
+        if (status) {
+            tasks = tasks.filter((task) => task.status === status);
+        }
+        // realiza a pesquisa
+        if (search) {
+            tasks = tasks.filter((task) => {
+                if (task.title.includes(search) || task.description.includes(search)) {
+                    return true;
+                }
+                return false;
+            })
+        }
+
+        return tasks;
+    }
+
+
     //criando o metodo de criacao de id onde ele verifica se o id e igual listado 
     getTaskById(id: string): Task {
         return this.tasks.find((task) => task.id === id)
@@ -31,6 +55,18 @@ export class TasksService {
         };
 
         this.tasks.push(task);
+        return task;
+    }
+    // metodo delete - retorna void (nulo) pois nao ira retornar valor
+    deleteTask(id: string): void {
+
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
+
+    // metodo update task
+    updateTaskStatus(id: string, status: TaskStatus) {
+        const task = this.getTaskById(id);
+        task.status = status;
         return task;
     }
 
