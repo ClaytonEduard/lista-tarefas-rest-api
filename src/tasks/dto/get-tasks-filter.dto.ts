661e39b385b58d2a703d54d659d9dd104345abd7
claytoneduard <clayton_eduard@hotmail.com>
2022-05-26 21:24:36 -0300

    Iniando configurações com o Docker

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -2,13 +2,12 @@ import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../tasks.model";
 
 export class GetTasksFilterDto {
-    // validacao sabendo que os dois metodos de pesquisa sao opcionais
-    // aplicar regras usando Enum
-  //  @IsOptional()
-    @IsEnum(TaskStatus)
-    status?: TaskStatus;
-    // queremos saber se é string mesmo
-  //  @IsOptional()
-    @IsString()
-    search?: string;
+  // validacao sabendo que os dois metodos de pesquisa sao opcionais
+  // aplicar regras usando Enum
+  @IsOptional()
+  @IsEnum(TaskStatus) status?: TaskStatus;
+  // queremos saber se é string mesmo
+  @IsOptional()
+  @IsString()
+  search?: string;
 }
