d48c687b1263708076e8a37a3be206d3b0fe5739
claytoneduard <clayton_eduard@hotmail.com>
2022-05-28 20:37:55 -0300

    Configuracao do ORM postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// importar o mdulos do banco
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+
+    }),],
 })
-export class AppModule {}
+export class AppModule { }
