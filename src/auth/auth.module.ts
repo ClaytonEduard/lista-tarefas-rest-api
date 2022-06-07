58c660172230baa9e2117e4660b9ce099e2e849a
claytoneduard <clayton_eduard@hotmail.com>
2022-06-07 13:14:58 -0300

    Finalizando modulo erro de senha com jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UsersRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -17,8 +18,10 @@ import { JwtModule } from '@nestjs/jwt';
       },
 
     }),
-    TypeOrmModule.forFeature([UsersRepository],)],
-  providers: [AuthService],
+    TypeOrmModule.forFeature([UsersRepository],
+      )],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule { }
